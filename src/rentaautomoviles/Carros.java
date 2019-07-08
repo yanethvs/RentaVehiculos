7bc3171b1f50ad4a428bce9c2334dd5ca3d500e2
unknown <Usuario@B6-501-18224.PASCUALBRAVO.CENTRAL>
2019-07-08 17:57:46 -0500

    actualización

---
+++ src/rentaautomoviles/Carros.java
@@ -0,0 +1,21 @@
+
+package rentaautomoviles;
+
+public class Carros 
+{
+   public String [] placas = {"FGT456","AZB321","MUF407","XLS777","DOG682"};
+   public String [] modelos = {"R4","Ferrari","Mazda323","lamborghini","BMW"};
+   public double [] precios = {100000,20000,50000,600000,1200000};
+   public boolean[] disponible = {true,true,true,true,true};
+}
+
+
+
+
+
+
+
+
+
+
+
